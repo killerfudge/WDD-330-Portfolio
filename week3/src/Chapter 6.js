790946945772f8cc0dda9d40a10e541d67db9514
killerfudge <trem_fan@yahoo.com>
2020-05-08 15:59:55 -0500

    week 3 reading

--- week3/src/Chapter 6.js
+++ week3/src/Chapter 6.js
@@ -1,14 +1,22 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Chapter 5 Example</title>
-    <script>
         const quiz = [
             { name: "Superman",realName: "Clark Kent" },
             { name: "Wonder Woman",realName: "Diana Prince" },
             { name: "Batman",realName: "Bruce Wayne" },
         ];
+
+        // View Object
+        const view = {
+            score: document.querySelector('#score strong'),
+            question: document.getElementById('question'),
+            result: document.getElementById('result'),
+            info: document.getElementById('info'),
+            render(target,content,attributes) {
+                for(const key in attributes) {
+                    target.setAttribute(key, attributes[key]);
+                }
+                target.innerHTML = content;
+            }
+        };
 
         const game = {
             start(quiz){
@@ -26,26 +34,24 @@
             },
             ask(){
                 const question = `What is ${this.question.name}'s real name?`;
+                view.render(view.question,question);
                 const response =  prompt(question);
                 this.check(response);
             },
             check(response){
                 const answer = this.question.realName;
                 if(response === answer){
+                    view.render(view.result,'Correct!',{'class':'correct'});
                     alert('Correct!');
                     this.score++;
+                    view.render(view.score,this.score);
                 } else {
+                    view.render(view.result,`Wrong! The correct answer was ${answer}`,{'class':'wrong'});
                     alert(`Wrong! The correct answer was ${answer}`);
                 }
             },
             gameOver(){
-                alert(`Game Over, you scored ${this.score} point${this.score !== 1 ? 's' : ''}`);
+                view.render(view.info,`Game Over, you scored ${this.score} point${this.score !== 1 ? 's' : ''}`);
             }
         }
         game.start(quiz);
-    </script>
-</head>
-<body>
-    <li><a href="../index.html">back</a></li>
-</body>
-</html>
