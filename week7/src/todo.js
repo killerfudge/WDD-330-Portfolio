4a12109fa8d5e1befbc2170bf5a1c02a969fac6d
killerfudge <trem_fan@yahoo.com>
2020-06-05 15:59:52 -0500

    week 7 chapter 13 as book

--- week7/src/todo.js
+++ week7/src/todo.js
@@ -2,26 +2,22 @@ const form = document.forms['todo'];
 form.addEventListener('submit', addTask, false);
 function addTask(event) {
     event.preventDefault();
-    const number = form.task.value;
-    const task = {
-        userId: 1,
-        title: form.task.value,
-        completed: false
-    }
-    const data = JSON.stringify(task);
-    const url = 'https://jsonplaceholder.typicode.com/todos';
+    const task = new FormData(form);
+    const url = `http://echo.jsontest.com/id/1/title/${form.task.value}`;
     const headers = new Headers({
         'Accept': 'application/json',
         'Content-Type': 'application/json'
-    })
+    });
     const request = new Request(url,
     {
         method: 'POST',
+        mode: 'cors',
         header: headers,
-        body: data
-    })
+        body: JSON.stringify(task)
+    }
+    )
     fetch(request)
     .then( response => response.json() )
-    .then( task => console.log(`Task saved with an id of ${task.id}`) )
+    .then( data => console.log(`${data.title} saved with an id of ${data.id}`) )
     .catch( error => console.log('There was an error:', error))
 }
