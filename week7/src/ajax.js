bfec84646611d1fd4fd4e7b077589e5e243482ae
killerfudge <trem_fan@yahoo.com>
2020-06-05 15:03:23 -0500

    week 7 ajax example

---
+++ week7/src/ajax.js
@@ -0,0 +1,38 @@
+//Create element variables
+const textButton = document.getElementById('number');
+const apiButton = document.getElementById('chuck');
+const outputDiv = document.getElementById('output');
+
+//Create URL variables
+const textURL = 'http://numbersapi.com/random';
+const apiURL = 'https://api.chucknorris.io/jokes/random';
+
+//Event for when the button for a number fact is clicked
+textButton.addEventListener('click', () => {
+    fetch(textURL)
+    .then( response => {
+        outputDiv.innerHTML = 'Waiting for response...';
+        if(response.ok) {
+            return response;
+        }
+        throw Error(response.statusText);
+    })
+    .then( response => response.text() )
+    .then( text => outputDiv.innerText = text )
+    .catch( error => console.log('There was an error:', error))
+},false);
+
+//Event for when the button for a Chuck Norris fact is clicked
+apiButton.addEventListener('click', () => {
+    fetch(apiURL)
+    .then( response => {
+        outputDiv.innerHTML = 'Waiting for response...';
+        if(response.ok) {
+            return response;
+        }
+        throw Error(response.statusText);
+    })
+    .then( response => response.json() )
+    .then( data => outputDiv.innerText = data.value )
+    .catch( error => console.log('There was an error:', error))
+},false);
