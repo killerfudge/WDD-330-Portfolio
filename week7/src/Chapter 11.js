8d17e6eaa0eaa28cd4d05cd753083cb5a62ce40b
killerfudge <trem_fan@yahoo.com>
2020-06-05 13:16:56 -0500

    week 7 chapter 11

--- week7/src/Chapter 11.js
+++ week7/src/Chapter 11.js
@@ -17,9 +17,6 @@ const quiz = [
     { name: "Superman",realName: "Clark Kent" },
     { name: "Wonder Woman",realName: "Diana Prince" },
     { name: "Batman",realName: "Bruce Wayne" },
-    { name: "The Hulk",realName: "Bruce Banner" },
-    { name: "Spider-man",realName: "Peter Parker" },
-    { name: "Cyclops",realName: "Scott Summers" }
 ];
 
 // View Object
@@ -50,15 +47,17 @@ const view = {
         this.render(this.score,game.score);
         this.render(this.result,'');
         this.render(this.info,'');
+        this.resetForm();
+    },
+    resetForm(){
+        this.response.answer.value = '';
+        this.response.answer.focus();
     },
     teardown(){
         this.hide(this.question);
         this.hide(this.response);
         this.show(this.start);
-    },
-    buttons(array){
-        return array.map(value => `<button>${value}</button>`).join('');
-    },
+    }
 };
 
 const game = {
@@ -69,23 +68,19 @@ const game = {
         this.ask();
     },
     ask(name){
-        console.log('ask() invoked');
-        if(this.questions.length > 2) {
+        if(this.questions.length > 0) {
             shuffle(this.questions);
             this.question = this.questions.pop();
-            const options = [this.questions[0].realName, this.questions[1].realName, this.question.realName];
-            shuffle(options);
             const question = `What is ${this.question.name}'s real name?`;
             view.render(view.question,question);
-            view.render(view.response,view.buttons(options));
         }
         else {
             this.gameOver();
         }
     },
     check(event){
-        console.log('check(event) invoked');
-        const response = event.target.textContent;
+        event.preventDefault();
+        const response = view.response.answer.value;
         const answer = this.question.realName;
         if(response === answer){
             view.render(view.result,'Correct!',{'class':'correct'});
@@ -94,6 +89,7 @@ const game = {
         } else {
             view.render(view.result,`Wrong! The correct answer was ${answer}`,{'class':'wrong'});
         }
+        view.resetForm();
         this.ask();
     },
     gameOver(){
@@ -102,4 +98,5 @@ const game = {
     }
 }
 view.start.addEventListener('click', () => game.start(quiz), false);
-view.response.addEventListener('click', (event) => game.check(event), false);
+view.response.addEventListener('submit', (event) => game.check(event), false);
+view.hide(view.response);
